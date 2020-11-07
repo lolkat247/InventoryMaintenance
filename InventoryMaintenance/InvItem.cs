dfb4e6bba13a49202c3adae9439ada06298b2493
Jacob Iglesias <jacob.ds.iglesias.5@gmail.com>
2020-11-06 21:06:56 -0500

    Add project files.

---
+++ InventoryMaintenance/InvItem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMaintenance
+{
+    public class InvItem
+    {
+        private int itemNo;
+        public int ItemNo
+        {
+            get { return itemNo; }
+            set { itemNo = value; }
+        }
+
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private decimal price;
+        public decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public string GetDisplayText()
+        {
+            string formattedItemNo = Convert.ToString(itemNo);
+            while (formattedItemNo.Length < 7)
+            {
+                formattedItemNo = "0" + formattedItemNo;
+            }
+
+            return (
+                formattedItemNo +
+                "    " +
+                description +
+                " (" + string.Format("{0:C}", price) + ")"
+                );
+        }
+
+        public InvItem()
+        {
+            itemNo = 0;
+            description = "New Item";
+            price = 0.0m;
+        }
+
+        public InvItem(int _itemNo, string _description, decimal _price)
+        {
+            itemNo = _itemNo;
+            description = _description;
+            price = _price;
+        }
+    }
+}
---
+++ InventoryMaintenance/frmInvMaint.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryMaintenance
+{
+    public partial class frmInvMaint : Form
+	{
+		public frmInvMaint()
+		{
+			InitializeComponent();
+		}
+
+		// Add a statement here that declares the list of items.
+		List<InvItem> items;
+
+		private void frmInvMaint_Load(object sender, EventArgs e)
+		{
+			// Add a statement here that gets the list of items.
+			items = InvItemDB.GetItems();
+			FillItemListBox();
+		}
+
+		private void FillItemListBox()
+		{
+			lstItems.Items.Clear();
+			// Add code here that loads the list box with the items in the list.
+			foreach (var x in items)
+			{
+				lstItems.Items.Add(x.GetDisplayText());
+			}
+		}
+
+		private void btnAdd_Click(object sender, EventArgs e)
+		{
+			// Add code here that creates an instance of the New Item form
+			// and then gets a new item from that form.
+			InvItem newItem = new frmNewItem().GetNewItem();
+			if (newItem != null)
+            {
+				items.Add(newItem);
+				InvItemDB.SaveItems(items);
+				FillItemListBox();
+            }
+			
+		}
+
+		private void btnDelete_Click(object sender, EventArgs e)
+		{
+			int i = lstItems.SelectedIndex;
+			if (i != -1)
+			{
+				// Add code here that displays a dialog box to confirm
+				// the deletion and then removes the item from the list,
+				// saves the list of products, and refreshes the list box
+				// if the deletion is confirmed.
+				DialogResult d = MessageBox.Show("Are you sure you want to delete this entry?", "Alert!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+				if (d == DialogResult.Yes)
+				{
+					items.Remove(items[i]);
+					InvItemDB.SaveItems(items);
+					FillItemListBox();
+				}
+			}
+		}
+
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
